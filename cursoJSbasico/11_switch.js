29df3fb0381a9b6459cd4ed626f2ede039a32262
Luis Auz <luisauz.geek@gmail.com>
2021-02-06 12:17:24 -0500

    feat: understanding switch
    course: Curso básico de JS
    cap: 13

---
+++ cursoJSbasico/11_switch.js
@@ -0,0 +1,16 @@
+var number = 1
+
+switch (number) {
+    case 1:
+        console.log("Soy un 1")
+        break
+    case 10: 
+        console.log("Soy un 10")
+        break
+    case 100:
+        console.log("Soy un 100")
+        break
+    default:
+        console.log("No soy nada")
+    }
+//FIXME: No olvidarse de utilizar el break al final de cada case
