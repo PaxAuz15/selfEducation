54c1f9279bacd0199f0277670212f60839c98d41
Luis Auz <luisauz.geek@gmail.com>
2021-02-06 11:15:08 -0500

    feat: understanding operators on JS
    couse: Curso básico de JS
    cap: 11

---
+++ cursoJSbasico/8_operadores.js
@@ -0,0 +1,35 @@
+// OPERACIONES BINARIOS
+//aritmetica
+3 + 2
+50 - 10
+10 * 20
+20 / 2 
+
+"Luis " + "Auz" //concatenacion
+
+// OPERADORES UNITARIOS
+!false //negacion
+
+// OPERADORES DE ASIGNACION
+var a = 1; // igual (=)
+
+//OPERADORES DE COMPARACION
+3 == "3" // compara el valor (==) //TRUE
+
+3 === "3" // valida tipos (number & string) y valores(3 & 3) sean estrictamente iguales (===) //FALSE
+
+5 < 3 // menor //FALSE
+5 > 3 // mayor //TRUE
+5 <= 6 //menor o igual //TRUE
+5 >= 6 //mayor o igual //FALSE
+a && b // si a es verdad Y si b es verdad =TRUE
+a || b // si a es verdad O si b es verdad =TRUE
+
+var edad = 22;
+
+edad++ //El valor se incrementa en uno
+
+console.log(edad) //23
+
+edad += 2 //El valor se incrementa segun el valor que se agrega
+console.log(edad) //24
