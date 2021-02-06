240c0d31f521ceb1f1d1cddd7c243a111a22fc0d
Luis Auz <luisauz.geek@gmail.com>
2021-02-06 09:19:47 -0500

    feat: variable types on JS
    course: Curso Básico de JS
    cap: 4

---
+++ cursoJSbasico/variables.js
@@ -0,0 +1,18 @@
+var nombre = "Luis";
+
+// Declarar variables
+var edad;
+
+// Inicializar la variable 
+edad = 30;
+
+edad
+
+var elementos = [nombre,edad]
+
+var persona = {
+    nombre,
+    edad
+}
+
+elementos[0]
