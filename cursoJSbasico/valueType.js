cb2f880c59efb5bf3c9d4f544095cce53480af36
Luis Auz <luisauz.geek@gmail.com>
2021-02-06 09:19:02 -0500

    feat: value types on JS
    Curso: Curso Básico de JS - Platzi
    cap: 3

---
+++ cursoJSbasico/valueType.js
@@ -0,0 +1,16 @@
+// Valores Primitivos
+40
+"Esto es un string"
+
+true
+false
+
+null
+undefined //No es recomendado utilizarlo
+
+// Valores no Primitivos (ambos son objetos)
+[1,2,3] //array
+
+{
+    nombre: "Diego"
+}
