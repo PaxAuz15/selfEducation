55ad46edae69f0adc39577233da095065b78a5fc
Luis Auz <luisauz.geek@gmail.com>
2021-02-12 09:02:54 -0500

    feat: Understanding arrow functions on JS
    course: Fundamentos de JS
    cap: 14

--- 2 fundamentosJS/12_arrowFunctions.js
+++ 2 fundamentosJS/12_arrowFunctions.js
@@ -3,40 +3,58 @@ var luis = {
     edad: 22
 }
 
-var gabriel = {
-    nombre: "Gabriel",
-    edad: 18
+var ricky = {
+    nombre: "Ricky",
+    edad: 5
 }
 
 const MAYORIA_DE_EDAD = 18
 
-/*
+/* funcion normal
 function mayoriaDeEdad(persona) {
     return persona.edad >= MAYORIA_DE_EDAD
 }
 */
 
 // Esta es una funcion anonima
-const mayoriaDeEdad = function(persona){
-    return persona.edad >= MAYORIA_DE_EDAD
-}
+// const mayoriaDeEdad = function(persona){
+//     return persona.edad >= MAYORIA_DE_EDAD
+// }
 
 //Arrow functions
-const mayoriaDeEdad = (persona) => {
-    return persona.edad >= MAYORIA_DE_EDAD
-}
+// Junto con la funcion anonima, son casi lo mismo. En arrow functions pasa algo más
+// const mayoriaDeEdad = (persona) => {
+//     return persona.edad >= MAYORIA_DE_EDAD
+// }
 
-const mayoriaDeEdad = persona => {  //Si solo se tiene un parametro en la funcion, se puede obviar los parentesis
-    return persona.edad >= MAYORIA_DE_EDAD
-}
+//Si solo se tiene un parametro en la funcion, se puede obviar los parentesis
+// const mayoriaDeEdad = persona => {  
+//     return persona.edad >= MAYORIA_DE_EDAD
+// }
 
-const mayoriaDeEdad = persona => persona.edad >= MAYORIA_DE_EDAD
+//Si solo una funcion retorna algo, se omite las llaves y el return
+// const mayoriaDeEdad = persona => persona.edad >= MAYORIA_DE_EDAD 
+
+//Desestructuras más el parámetro
+//Si quiero un pasar un objeto que tiene un atributo edad, lo podría poner así!
+const mayoriaDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD
 
 function imprimirMSGmayorDeEdad(persona) {
     if (mayoriaDeEdad(persona)){
-        console.log(`${persona.nombre} es mayor de edad. Tiene ${edad} anios`)
+        console.log(`${persona.nombre} es mayor de edad. Tiene ${persona.edad} anios`)
     }else{
-    console.log(`${persona.nombre} no es mayor de edad. Tiene tan solo ${edad} anios`)
+    console.log(`${persona.nombre} no es mayor de edad. Tiene tan solo ${persona.edad} anios`)
+    }
+}
 
+// const permitirAcceso = (persona) => {
+//     if(!mayoriaDeEdad(persona)){
+//         console.log("ACCESO DENEGADO")
+//     }
+// }
+
+const permitirAcceso = persona => {
+    if(!mayoriaDeEdad(persona)){
+        console.log("ACCESO DENEGADO")
     }
 }
