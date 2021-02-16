8b2a9decd281f4340e1eee92c5110f96af5b79ef
Luis Auz <luisauz.geek@gmail.com>
2021-02-16 12:58:16 -0500

    feat: Understanding Sintaxys and Data Type on JS
    course: Desarrollo Web. Javascript desde cero. Edicion 2020
    cap: 4 (Sintaxis y tipos de datos)

---
+++ 3 javascript 2020 desde cero/1_sintaxisAndTipoDeDatos.js
@@ -0,0 +1,27 @@
+let numero; //declarar una variable
+
+numero = 5; // inicializar una variable
+
+let numero2 = 5; //declarar e inicializar
+
+const numero3 = 5; //las constantes solo se pueden declarar e inicializar en la misma sentencia
+
+//TODO: Tipo de Datos
+
+//Primitivos
+let numero5 = 2; //numeros
+let numeroDecimal = 1.5
+let numeroNegativo = -1.5
+
+let palabra = 'Hola'; let palabra2 = "hola"; //strings
+//booleanos
+let verdadero = true;
+let falso = false;
+
+//Undefined
+//Null
+//Symbol
+
+const PI = 3.14 //Por convecion, las constantes son en mayusculas
+
+console.log(PI)
