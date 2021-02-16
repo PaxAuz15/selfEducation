52623ca8216e52a77c2e3ce2726b62db3c4a9054
Luis Auz <luisauz.geek@gmail.com>
2021-02-16 14:49:55 -0500

    feat: Understanding Operators Type
    course: Desarrollo Web. Javascript desde cero. Version 2020
    cap: 5 (Tipos de Operadores)

---
+++ 3 javascript 2020 desde cero/2_tipoDeOperadores.js
@@ -0,0 +1,36 @@
+let a = 5
+let b = 2
+
+let suma = a + b
+let resta = a - b
+let multiplicacion = a * b
+let division = a / b
+let modulo = a % b
+
+// console.log(suma)
+// console.log(resta)
+// console.log(multiplicacion)
+// console.log(division.toFixed(2))
+// console.log(modulo)
+
+
+console.log(a)
+console.log(a++) //Incrementa en uno pero primero se devuelve la variable 
+console.log(a) // Por eso debo imprimir para notar el incremento
+console.log(++a) //Aqui se incrementa y devuleve la variable al mismo tiempo
+// console.log(a)
+
+console.log(a--) //Decrementa en uno pero primero se devuelve la variable 
+console.log(a) // Por eso debo imprimir para notar el decremento
+console.log(--a) //Aqui se decrementa y devuleve la variable al mismo tiempo
+
+//TODO: Operadores de asignacion
+
+let c = 9
+let d = 2
+
+console.log(c+=3) //c = c+3
+console.log(c-=3) //c = c-3
+console.log(c*=3) //c = c*3
+console.log(c/=3) //c = c/3
+console.log(c%=d) //c = c%3
