42cc56e2cbe0b3d312b7cd5d63ae75417974a4c1
Luis Auz <luisauz.geek@gmail.com>
2021-02-27 16:36:31 -0500

    feat: npm install @syncfusion/ej2-react-grids --save comment: default code on react app

--- Syncfusion/1_getting-started-data-grid-react/src/App.js
+++ Syncfusion/1_getting-started-data-grid-react/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {GridComponent} from '@syncfusion/ej2-react-grids';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <GridComponent/>
+    </div>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
