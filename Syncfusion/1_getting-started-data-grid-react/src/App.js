8222c541441f60a11f247f662b3118ce3f696d18
Luis Auz <luisauz.geek@gmail.com>
2021-02-27 17:35:52 -0500

    feat: Use features on GridComponent 	- ColumnDirective 	- ColumnsDirective 	- Page (Pagination) 	- Filter 	- Group 	- Inject

--- Syncfusion/1_getting-started-data-grid-react/src/App.js
+++ Syncfusion/1_getting-started-data-grid-react/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import {GridComponent} from '@syncfusion/ej2-react-grids';
+import {GridComponent, ColumnDirective, ColumnsDirective, Inject, Page, Filter, Group} from '@syncfusion/ej2-react-grids';
 import data from './dataSource.json'
 
 function App() {
   return (
-    <div>
-      <GridComponent dataSource={data}/>
+    <div className="TableOnData">
+      <GridComponent 
+      dataSource={data} 
+      className='table' 
+      allowPaging={true}
+      pageSettings={{ pageSize:6}}
+      allowFiltering={true}
+      allowGrouping={true}>
+        <ColumnsDirective>
+          <ColumnDirective field='usuario_id' headerText='Id' width='25%'/>
+          <ColumnDirective field='usuario_login' headerText='Login' width='50%'/>
+          <ColumnDirective field='nombres' headerText='Nombres' width='75%'/>
+          <ColumnDirective field='primer_apellido' headerText='Apellidos' width='75%'/>
+          <ColumnDirective field='codigo_estado' headerText='Estado' width='25%'/>
+          <ColumnDirective field='fecha_actualizacion' headerText='Actualización' width='75%'/>
+        </ColumnsDirective>
+        <Inject services={[Page, Filter, Group]}/>
+      </GridComponent>
     </div>
     /*<div className="App">
       <header className="App-header">
