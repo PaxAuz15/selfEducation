e4c8518adb202452c283f1dee33c76563b9a39d0
Luis Auz <luisauz.geek@gmail.com>
2021-02-27 18:23:03 -0500

    feat: change local json to remote json

--- Syncfusion/2_binding_local_and_remote_json/src/App.js
+++ Syncfusion/2_binding_local_and_remote_json/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import {GridComponent, ColumnDirective, ColumnsDirective, Inject, Page, Filter, Group} from '@syncfusion/ej2-react-grids';
+import { DataManager, ODataV4Adaptor } from "@syncfusion/ej2-data";
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const data = new DataManager({
+    url:'https://services.odata.org/V4/Northwind/Northwind.svc/Orders',
+    adaptor: new ODataV4Adaptor()
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GridComponent
+        dataSource={data}
+        className='table'
+        allowPaging={true}
+        pageSettings={{ pageSize:6}}
+        allowFiltering={true}
+        allowGrouping={true}>
+        <ColumnsDirective>
+          <ColumnDirective field='OrderID' width='100' textAlign="Right" />
+          <ColumnDirective field='CustomerID' width='100' />
+          <ColumnDirective field='EmployeeID' width='100' textAlign="Right" />
+          <ColumnDirective field='Freight' width='100' format="C2" textAlign="Right" />
+          <ColumnDirective field='ShipCountry' width='100' />
+        </ColumnsDirective>
+        <Inject services={[Page, Filter, Group]}/>
+      </GridComponent>
     </div>
-  );
+  )
 }
 
 export default App;
