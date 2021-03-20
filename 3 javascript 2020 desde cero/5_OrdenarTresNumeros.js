b3335f44c753552f8f8a52a34e00ace777c5692b
Luis Auz <luisauz.geek@gmail.com>
2021-03-19 19:02:01 -0500

    Fix: Show answer on div

--- 3 javascript 2020 desde cero/5_OrdenarTresNumeros.js
+++ 3 javascript 2020 desde cero/5_OrdenarTresNumeros.js
@@ -5,41 +5,46 @@ const result = document.getElementById('result');
 let numero1 = prompt('Introduzca el primer numero')
 let numero2 = prompt('Introduzca el segundo numero')
 let numero3 = prompt('Introduzca el tercer numero')
-let choice = prompt('Ingrese 0 si quiere ascendente y 1 si quiere descendente')
+let choice = prompt('Ingrese 0 si quiere descendente y 1 si quiere ascendente')
 
-if(choice === 0){
-    if (numero1 < numero2 && numero2 < numero3){
-        numbers.textContent = `${numero3} - ${numero2} - ${numero1}`;
-        if (numero1 < numero2 && numero3 < numero2){
-            numbers.textContent = `${numero2} - ${numero3} - ${numero1}`;
+if(choice == 0){
+    if(numero1 <= numero2 && numero1 <= numero3){
+        if(numero2 <= numero3){
+            result.textContent = `${numero2} - ${numero3} - ${numero1}`;
+        }else if (numero3 <=  numero2){
+            result.textContent = `${numero3} - ${numero2} - ${numero1}`;
         }
-    } else if (numero2 < numero1 && numero1 < numero3){
-        numbers.textContent = `${numero3} - ${numero1} - ${numero2}`;
-        if (numero2 < numero1 && numero3 < numero1){
-            numbers.textContent = `${numero1} - ${numero3} - ${numero2}`;
+    }else if(numero2 <= numero1 && numero2 <= numero3){
+        if(numero1 <= numero3){
+            result.textContent = `${numero3} - ${numero1} - ${numero2}`;
+        }else if (numero3 <=  numero1){
+            result.textContent = `${numero1} - ${numero3} - ${numero2}`;
         }
-    } else if (numero3 < numero1 && numero1 < numero2){
-        numbers.textContent = `${numero2} - ${numero1} - ${numero3}`;
-        if (numero3 < numero1 && numero2 < numero1){
-            numbers.textContent = `${numero1} - ${numero2} - ${numero3}`;
+    }else if(numero3 <= numero1 && numero3 <= numero2){
+        if(numero2 <= numero1){
+            result.textContent = `${numero1} - ${numero2} - ${numero3}`;
+        }else if (numero1 <=  numero2){
+            result.textContent = `${numero2} - ${numero1} - ${numero3}`;
         }
     }
-
-}else if (choice === 1) {
-    if (numero1 > numero2 && numero2 > numero3) {
-        numbers.textContent =`${numero1} - ${numero2} - ${numero3}`;
-        if (numero1 > numero2 && numero3 > numero2) {
-            numbers.textContent = `${numero1} - ${numero3} - ${numero2}` ;
+}else if (choice == 1) {
+    if (numero1 >= numero2 && numero2 >= numero3){
+        if (numero2 >= numero3){
+            result.textContent = `${numero3} - ${numero2} - ${numero1}`;
+        }else if(numero3 >= numero2){
+            result.textContent = `${numero2} - ${numero3} - ${numero1}`;
         }
-    }else if (numero2 > numero1 && numero1 > numero3){
-        numbers.textContent = `${numero2} - ${numero1} - ${numero3}`;
-        if (numero2 > numero1 && numero3 > numero1){
-            numbers.textContent = `${numero2} - ${numero3} - ${numero1}`;
+    }else if (numero2 >= numero1 && numero2 >= numero3){
+        if (numero1 >= numero3){
+            result.textContent = `${numero3} - ${numero1} - ${numero2}`;
+        }else if (numero3 >= numero1){
+            result.textContent = `${numero1} - ${numero3} - ${numero2}`;
         }
-    }else if (numero3 > numero1 && numero1 > numero2){
-        numbers.textContent = `${numero3} - ${numero1} - ${numero2}`;
-        if (numero3 > numero1 && numero2 > numero1){
-            numbers.textContent =`${numero3} - ${numero2} - ${numero1}`;
+    }else if (numero3 >= numero1 && numero3 >= numero2){
+        if (numero1 >= numero2){
+            result.textContent = `${numero2} - ${numero1} - ${numero3}`;
+        } else if (numero2 >= numero1){
+            result.textContent = `${numero1} - ${numero2} - ${numero3}`;
         }
     }
 }
