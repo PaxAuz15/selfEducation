3c373c01d3901687738383a2f8e971ad0d777154
Luis Auz <luisauz.geek@gmail.com>
2021-03-29 21:22:42 -0500

    Exercise 9 finished

---
+++ 3 javascript 2020 desde cero/17_Ejercicios de repaso/ejercicio9.js
@@ -0,0 +1,27 @@
+const MSG_INPUT="Ingrese una palabra";
+const VOCALES = ['a','e','i','o','u'];
+const SEPARADOR = ""
+
+let i=0;
+let iVocales =0;
+let iConsonantes =0;
+
+let div = document.getElementById('div-respuesta');
+let inputUser = prompt(MSG_INPUT).toLowerCase();
+
+let sacarEspacios = inputUser.trim();
+
+let arregloDeInput = sacarEspacios.split(SEPARADOR);
+
+for(i;i<arregloDeInput.length;i++){
+    if((VOCALES.indexOf(arregloDeInput[i]))==-1){
+        iConsonantes++;
+    }else{
+        iVocales++;
+    }
+}
+
+div.textContent = `La cantidad de vocales de la palabra ${inputUser} es ${iVocales} y el número de consonantes es ${iConsonantes}`;
+
+
+
