8c989ec74c6a85343d62f6c02bcbfe0e1b65a777
Luis Auz <luisauz.geek@gmail.com>
2021-05-10 17:19:30 -0500

    First Part to Attributes on DOM

--- 3 javascript 2020 desde cero/DOM/3_Attributes/main.js
+++ 3 javascript 2020 desde cero/DOM/3_Attributes/main.js
@@ -8,5 +8,26 @@ title.setAttribute('class','new class')
 console.log(title.getAttribute('class'));
 
 // CLASS
-title.classList.add("main-title");
-console.log(title.classList);
+classTitle = title.classList;
+classTitle.add("main-title");
+console.log(classTitle);
+classTitle.add("other-class");
+console.log(classTitle);
+
+if(classTitle.contains("other-class")){
+    classTitle.remove("other-class");
+    console.log(classTitle);
+}else{
+    classTitle.add("other-class");
+    console.log(classTitle);
+}
+
+classTitle.replace("main-title","second-title");
+console.log(classTitle);
+
+// DIRECT ATTRIBUTES
+// TODO: Hay elementos que se pueden apreciar directamente del DOM
+console.log(title.id);
+console.log(title.innerHTML); // -> Este devuelve el codigo dentro de la etiqueta
+console.log(title.textContent); // -> Este devuelve el texto plano dentro de la etiqueta
+console.log(name.value);
