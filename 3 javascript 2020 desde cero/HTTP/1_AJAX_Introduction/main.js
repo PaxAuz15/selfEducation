a0638a28b864fca89ebcf19fc581cf30c0a25d60
Luis Auz <luisauz.geek@gmail.com>
2021-05-19 20:02:16 -0500

    Not yet first lesson on Ajax http request

---
+++ 3 javascript 2020 desde cero/HTTP/1_AJAX_Introduction/main.js
@@ -0,0 +1,8 @@
+const list_user = document.getElementById('list_user');
+const list_post = document.getElementById('list_post');
+const get_users = document.getElementById('get_users');
+const get_posts = document.getElementById('get_posts');
+
+get_users.addEventListener('click',()=>{
+    
+})
