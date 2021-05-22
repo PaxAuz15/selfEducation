5b1574a382796815b9160b6744ff868c91d07ef7
Luis Auz <luisauz.geek@gmail.com>
2021-05-21 22:35:51 -0500

    Get Users

--- 3 javascript 2020 desde cero/HTTP/1_AJAX_Introduction/main.js
+++ 3 javascript 2020 desde cero/HTTP/1_AJAX_Introduction/main.js
@@ -4,5 +4,25 @@ const get_users = document.getElementById('get_users');
 const get_posts = document.getElementById('get_posts');
 
 get_users.addEventListener('click',()=>{
-    
-})
+    let xhr;
+    if(window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.open('GET','https://jsonplaceholder.typicode.com/users');
+
+    xhr.addEventListener('load', (data)=>{
+        const dataUser = JSON.parse(data.target.response);
+        console.log(dataUser);
+
+        for (const userInfo of dataUser){
+            const listUser = document.createElement('LI');
+            listUser.textContent = `${userInfo.id} - ${userInfo.name}`;
+            list_user.appendChild(listUser);
+        }
+    })
+
+    xhr.send()
+});
