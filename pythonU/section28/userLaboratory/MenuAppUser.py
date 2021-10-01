06aaa8184bccd01b425c1cf6d733fd754dd12ead
Luis Auz <luisauz.geek@gmail.com>
2021-10-01 09:46:03 -0500

    ref: refactoring section 28 - Python: Final Laboratory - Data Layer - User case (my interpretion)

--- pythonU/section28/userLaboratory/MenuAppUser.py
+++ pythonU/section28/userLaboratory/MenuAppUser.py
@@ -1,5 +1,6 @@
 from UserDao import UserDao
 from User import User
+from logger_base import log
 
 menu:str="""1) User List\n2) Add User\n3) Update User\n4) Delete User\n5) Exit\nWrite your option: """
 choiceUser = None
@@ -7,23 +8,33 @@ choiceUser = None
 while choiceUser != 5:
     choiceUser:int = int(input(menu))
     if choiceUser == 1:
-        UserDao.getAllUsers()
+        users = UserDao.getAllUsers()
+        for user in users:
+            log.info(user)
     elif choiceUser == 2:
         username:str = input("What's your username: ")
         password:str = input("What's your password: ")
         user:User = User(username=username,password=password)
-        UserDao.insertUser(user)
+        userInserted = UserDao.insertUser(user)
+        log.info(f"User inserted: {userInserted}")
     elif choiceUser == 3:
-        UserDao.getAllUsers()
+        users = UserDao.getAllUsers()
+        for user in users:
+            log.info(user)
         idUser:int = int(input("Write id user to update: "))
         username:str = input("What's your username: ")
         password:str = input("What's your password: ")
         user:User = User(idUser,username,password)
-        UserDao.updateUser(user)
+        userUpdated = UserDao.updateUser(user)
+        log.info(f"User updated: {userUpdated}")
     elif choiceUser == 4:
-        UserDao.getAllUsers()
+        users = UserDao.getAllUsers()
+        for user in users:
+            log.info(user)
         idUser:int = int(input("Write id user to delete: "))
         user:User = User(idUser=idUser)
-        UserDao.deleteUser(user)
+        userDeleted = UserDao.deleteUser(user)
+        log.info(f"User deleted: {userDeleted}")
     else:
+        log.info("App Exit")
         continue
