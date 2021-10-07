79c446c5cf57e1d6b945190b35de3c3be7c8cc32
Luis Auz <luisauz.geek@gmail.com>
2021-10-07 09:29:53 -0500

    testing functions on UserDao

--- pythonU/section28 - Final Laboratory: User Data Layer/userLaboratory/logger_base.py
+++ pythonU/section28 - Final Laboratory: User Data Layer/userLaboratory/logger_base.py
@@ -3,7 +3,7 @@ import logging as log
 log.basicConfig(
     level=log.INFO,
     format="%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s",
-    datefmt="%I:%M:S",
+    datefmt="%I:%M:%S",
     handlers=[
         log.FileHandler("data_layer.log"),
         log.StreamHandler()
