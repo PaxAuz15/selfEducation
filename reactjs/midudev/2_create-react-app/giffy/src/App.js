beeea15f3c5e01ccf15902950ac646ed1eefa00c
Luis Auz <luisauz.geek@gmail.com>
2022-02-04 17:51:35 -0500

    feat: separate codes

--- reactjs/midudev/2_create-react-app/giffy/src/App.js
+++ reactjs/midudev/2_create-react-app/giffy/src/App.js
@@ -1,30 +1,12 @@
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
-import { useState } from 'react';
 
 const App = () => {
-  const [keyword,setKeyword] = useState("")
-  const [change,setChange] = useState("")
-
-  function handleChange(event){
-    setChange(event.target.value)
-  }
-
-  function handleSubmit(event){
-    event.preventDefault()
-    setKeyword(event.target[0].value)
-  }
-
   return (
     <div className="App">
       <section className="App-content">
         {console.log("render")}
-        <form onSubmit={handleSubmit}>
-            {console.log("render from FormGif")}
-          <input onChange={handleChange} value={change}></input>
-          <button>Search Gif</button>
-        </form>
-        <ListOfGifs keyword={keyword} />
+        <ListOfGifs keyword="Chile" />
       </section>
     </div>
   );
