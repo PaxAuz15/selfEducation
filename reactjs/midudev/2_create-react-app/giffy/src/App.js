1a171f483c167f2610f0d2193aa0ef9851217a11
Luis Auz <luisauz.geek@gmail.com>
2022-02-04 11:57:55 -0500

    understand useEffect & useState

--- reactjs/midudev/2_create-react-app/giffy/src/App.js
+++ reactjs/midudev/2_create-react-app/giffy/src/App.js
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const GIFS = [
+  "https://media.giphy.com/media/7frSUXgbGqQPKNnJRS/giphy.gif",
+  "https://media.giphy.com/media/z0ImSbMh4uJR4BEJju/giphy.gif"
+]
+
+const GIFS_DIFF = [
+  "https://media.giphy.com/media/MVDPX3gaKFPuo/giphy.gif",
+  "https://media.giphy.com/media/26xBwFcBGPLNfGbkY/giphy.gif"
+]
+
+const App = () => {
+  const [gifs,setGif] = useState(GIFS)
+
+  function handleChangeGifs(){
+    setGif(GIFS_DIFF)
+  }
+
+  useEffect(function(){
+    console.log("useEffect")
+    setGif(GIFS_DIFF)
+  },[])
+
   return (
     <div className="App">
       <section className="App-content">
-        gifs
+        {console.log("render")}
+        {
+          gifs.map(gif=>{
+            return <img src={gif}/>
+          })
+        }
+        <button onClick={handleChangeGifs}>Change gifs</button>
       </section>
     </div>
   );
