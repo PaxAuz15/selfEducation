d739dfa821be929fcaf69ee5ef8c608de0242236
Luis Auz <luisauz.geek@gmail.com>
2022-02-05 06:22:08 -0500

    feat: create loading state & component

--- reactjs/midudev/2_create-react-app/giffy/src/components/ListOfGifs.js
+++ reactjs/midudev/2_create-react-app/giffy/src/components/ListOfGifs.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react"
 import Gif from "./Gif"
 import getGifs from "../services/getGifs"
+import Loading from "./Loading"
 
 const ListOfGifs = ({params}) => {
     const {keyword} = params
     const [gifs, setGifs] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(function(){
         console.log("useEfect ListOfGifs");
+        setLoading(true)
         getGifs({keyword})
-            .then(gifs=>setGifs(gifs))
+            .then(gifs=>{
+                setGifs(gifs)
+                setLoading(false)
+            })
     },[keyword])
     
     return (
         <>
         {console.log("render from ListOfGifs")}
-        {gifs.map(({id,title,url})=> {
+        {loading ? <Loading /> : gifs.map(({id,title,url})=> {
             return <Gif key={id} id-={id} title={title} image={url} />
         })}
         </>
---
+++ reactjs/midudev/2_create-react-app/giffy/src/components/Loading.js
@@ -0,0 +1,5 @@
+const Loading = () => {
+    return <h1>Loading 🥨</h1>
+}
+
+export default Loading
