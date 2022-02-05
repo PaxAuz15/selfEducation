b48a2dfb6e31abb31a8f2519d7048080b1a37df2
Luis Auz <luisauz.geek@gmail.com>
2022-02-05 07:15:06 -0500

    feat: root page and adding some styles

--- reactjs/midudev/2_create-react-app/giffy/src/App.js
+++ reactjs/midudev/2_create-react-app/giffy/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 import { Route, Link } from 'wouter';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="App">
       <section className="App-content">
-        <h1>App</h1>
-        {/* spa with wouter -> Link Element */}
-        <Link to="/gif/colombia">Gif de Colombia</Link>
-        <Link to="/gif/ecuador">Gif de Ecuador</Link>
-        <Link to="/gif/chile">Gif de Chile</Link>
+        <h1>
+          <Link to='/'>App</Link>
+        </h1>
         {console.log("render")}
         <Route 
+          component={Home}
+          path="/"
+        />
+        <Route 
           component={ListOfGifs}
-          path="/gif/:keyword"/>
+          path="/search/:keyword"/>
       </section>
     </div>
   );
---
+++ reactjs/midudev/2_create-react-app/giffy/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import { Link } from "wouter"
+
+const POPULAR_GIFS = [
+    "Naruto",
+    "Morty",
+    "La casa de papel",
+    "React JS",
+    "Barcelona SC"
+]
+
+const Home = () => {
+    return (
+        <>
+        {
+            POPULAR_GIFS.map(gifCategory => {
+                return <Link key={gifCategory} to={`/search/${gifCategory}`}>{`Gifs de ${gifCategory}`}</Link>
+            })
+        }
+        </>
+    )
+}
+
+export default Home
