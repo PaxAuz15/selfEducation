fea54249cbb5fc66f08250990275968d1f9a76c9
Luis Auz <luisauz.geek@gmail.com>
2022-02-11 00:26:34 -0500

    feat: variables int & strings

--- javaU/Java/Fundamentos/HelloWorld/src/HelloWorld.java
+++ javaU/Java/Fundamentos/HelloWorld/src/HelloWorld.java
@@ -11,6 +11,26 @@
 //My class in Java
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        int myVarInteger = 10;
+        System.out.println(myVarInteger);
+        
+        myVarInteger = 5;
+        System.out.println(myVarInteger);
+        
+        String myVarString = "Hello";
+        System.out.println(myVarString);
+        
+        myVarString = "Bye";
+        System.out.println(myVarString);
+        
+        var num2 = 2;
+        System.out.println(num2);
+        
+        var string3 = "Luis";
+        System.out.println("String3 = "+string3);
+        
+        var myVar = 1;
+        var _myVar = 2;
+        var $myVar = 3;
     }
 }
